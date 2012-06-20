8eeb33ec296c52b82423866162923e8a2f629e90
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-06-20 13:13:10 +0200

    change controller order

--- lib/travis/admin.rb
+++ lib/travis/admin.rb
@@ -2,9 +2,9 @@ require 'forwardable'
 
 module Travis
   module Admin
-    require 'travis/admin/gh_controller'
     require 'travis/admin/main_controller'
     require 'travis/admin/repo_controller'
+    require 'travis/admin/gh_controller'
 
     extend SingleForwardable
     def_single_delegators Controller, :call, :settings, :new, :configure
